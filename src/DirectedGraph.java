34738775d46ada2d4dea1a9856f7a1785f14ebd6
A-Mulvihill <mulvihad@tcd.ie>
2021-10-24 14:18:07 +0100

    Changed the implementation of DirectedGraph such that each node only knows its parents instead of its children. Removed variables and methods that are not relevant to LCA.

--- src/DirectedGraph.java
+++ src/DirectedGraph.java
@@ -1,65 +1,23 @@
 import java.util.ArrayList;
 
 // This class is for a Directed Graph, but will only be used as a DAG
+// This is implemented where each node only knows its parents and not its children
 public class DirectedGraph
 {
-    private class Edge
-    {
-        int v, w;
-
-        protected Edge(int v, int w)
-        {
-            this.v = v;
-            this.w = w;
-        }
-
-        protected int from()
-        {
-            return v;
-        }
-
-        protected int to()
-        {
-            return w;
-        }
-    }
-
-    private final int V;
-    private ArrayList[] adj;
+    private ArrayList[] nodeParents;
 
     public DirectedGraph(int V)
     {
-        this.V = V;
-        adj = new ArrayList[V];
+        nodeParents = new ArrayList[V];
         for(int i = 0; i < V; i++)
         {
-            adj[i] = new ArrayList<Edge>();
+            nodeParents[i] = new ArrayList<Integer>();
         }
-    }
-
-    public int V()
-    {
-        return V;
-    }
-
-    public int E()
-    {
-        int result = 0;
-        for(int i = 0; i < V; i++)
-        {
-            result += adj[i].size();
-        }
-        return result;
     }
 
     public void addEdge(int from, int to)
     {
-        adj[from].add(new Edge(from, to));
-    }
-
-    public ArrayList adjacent(int v)
-    {
-        return adj[v];
+        nodeParents[to].add(from);
     }
 
     public ArrayList<Integer> LCAsOf(int a, int b)
